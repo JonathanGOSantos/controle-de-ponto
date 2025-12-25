b86b525e3dfbb1196e60a181c65d2412ccec2333
Jonathan Gustavo de Oliveira Santos <130310262+JonathanGOSantos@users.noreply.github.com>
2025-12-24 23:46:39 -0300

    Primeiro commit

---
+++ src/test/java/dev/santosjonathan/controle_de_ponto/ControleDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.santosjonathan.controle_de_ponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
